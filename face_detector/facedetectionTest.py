909a9865c65d7df28b29ece36454bcbc6a56ef12
ayulockin <mein2work@gmail.com>
2019-08-22 03:37:14 +0530

    crop faces

--- face_detector/facedetectionTest.py
+++ face_detector/facedetectionTest.py
@@ -15,20 +15,19 @@ print(result)
 # Result is an array with all the bounding boxes detected. We know that for 'ivan.jpg' there is only one.
 for detection in result:
     bounding_box = detection['box']
-    keypoints = detection['keypoints']
+
     cv2.rectangle(image,
               (bounding_box[0], bounding_box[1]),
               (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
-              (0,155,255),
-              2)
+              (0,155,255), 2)
 
-    # cv2.circle(image,(keypoints['left_eye']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['right_eye']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['nose']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['mouth_left']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
+
+cropped = image[bounding_box[1]: bounding_box[1] + bounding_box[3], bounding_box[0]:bounding_box[0]+bounding_box[2]]
 
 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+cropped = cv2.cvtColor(cropped, cv2.xCOLOR_RGB2BGR)
+
 cv2.imshow("frame", image)
+cv2.imshow("croppedframe", cropped)
 cv2.waitKey()
 
