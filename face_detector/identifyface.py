f042acadc05489515cfe6a74377220ab0e3f0d89
ayulockin <mein2work@gmail.com>
2019-08-26 12:05:01 +0530

    WARNINGS closed

--- face_detector/identifyface.py
+++ face_detector/identifyface.py
@@ -1,10 +1,12 @@
 import numpy as np 
 import pandas as pd
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
 import cv2
 
 import tensorflow as tf
+import tensorflow.python.util.deprecation as deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
 
 import keras.backend as K
 from keras.models import Model
